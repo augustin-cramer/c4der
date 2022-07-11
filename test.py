82b9bbefcc8f98f37054456e4b46b835bb0c3b90
g0bel1n <lucas.saban@icloud.com>
2022-07-11 18:30:15 +0200

    refactoring and stuff

--- test.py
+++ test.py
@@ -4,29 +4,29 @@ import pandas as pd
 import plotly.express as px
 from c4der._c4der import c4der
 
+
 def standardize_cluster_format(converter) -> dict:
-    i=0
-    for k,v in converter.items():
-        if v and k!= -1 :
+    i = 0
+    for k, v in converter.items():
+        if v and k != -1:
             converter[k] = i
-            i+=1
-        else :
-            converter[k]= -1
+            i += 1
+        else:
+            converter[k] = -1
 
     return converter
 
 
-if __name__ == '__main__' : 
+if __name__ == "__main__":
 
     df = pd.read_csv("test_set/3D_500_area.csv", index_col=0)
     df.Area = np.sqrt(df.Area)
-    start = datetime(year = 2022, month = 7,day=2, hour=9, minute=1)
-    df.loc[:,"T"] = df['T'].apply(lambda x: start + timedelta(seconds=x) 
+    start = datetime(year=2022, month=7, day=2, hour=9, minute=1)
+    df.loc[:, "T"] = df["T"].apply(lambda x: start + timedelta(seconds=x))
 
-########### c4der Time ###########
+    ########### c4der Time ###########
     c4der_scan = c4der(
-        50, -1, 70, 240, 10, spatial_weight_on_x=.2, 
-        non_spatial_epss=0.1
+        50, -1, 70, 240, 10, spatial_weight_on_x=0.2, non_spatial_epss=0.1
     )
 
     c4der_scan.fit(
@@ -36,14 +36,13 @@ if __name__ == '__main__' :
     )
     df["labels"] = c4der_scan.labels_
 
-###################################
+    ###################################
 
+    converter = standardize_cluster_format((df.groupby("labels").count().X > 20))
 
-    converter = standardize_cluster_format((df.groupby('labels').count().X > 20))
-    
-    df['labels'] = df['labels'].apply(lambda x: converter[x])
+    df["labels"] = df["labels"].apply(lambda x: converter[x])
 
-    df = df[df.labels != -1] # Get rid of the noise
+    df = df[df.labels != -1]  # Get rid of the noise
 
     fig = px.scatter_3d(
         z=df["T"],
@@ -54,8 +53,8 @@ if __name__ == '__main__' :
 
     fig.show()
 
-    print(f'Nombre de clients détectés :{len(df.labels.unique())}')
+    print(f"Nombre de clients détectés :{len(df.labels.unique())}")
     for cluster in df.labels.unique():
-        start = min(df[df.labels==cluster]['T'])
-        end = max(df[df.labels==cluster]['T'])
+        start = min(df[df.labels == cluster]["T"])
+        end = max(df[df.labels == cluster]["T"])
         print(f'{start.strftime("%H:%M")} -->  {end.strftime("%H:%M")}')
